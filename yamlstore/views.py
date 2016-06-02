d9d37d7e9cf841477e5b48dd0b41d6a033ea92fd
ericstalbot <est1209@gmail.com>
2016-06-01 19:49:09 -0600

    work on views and templates

--- yamlstore/views.py
+++ yamlstore/views.py
@@ -1,4 +1,5 @@
-from flask import session, request, redirect, url_for, render_template
+from flask import session, request, redirect, url_for, render_template, \
+    json, flash
 
 from flask_user import login_required, current_user
 
@@ -7,9 +8,8 @@ from yamlstore.models import YamlDocument, EditDocumentForm
 from yamlstore.yaml_handling import InvalidYaml, process_yaml
 
 
-@app.route('/')
-@login_required
-def view_documents():
+@app.route('/users/<int:user_id>/docs')
+def view_user_documents():
     return current_user.username
     #if logged in, show list of document titles
     #else, see other to log in page
@@ -22,12 +22,31 @@ def new_yaml_doc():
     db.session.add(doc)
     db.session.commit()
     redirect(url_for('edit_yaml_doc', doc_id=doc.id))
+
+
+@app.route('/docs/<int:doc_id>')
+def view_yaml_doc(doc_id):
+    doc = YamlDocument.query.get_or_404(doc_id) 
     
-@app.route('/edit/<int:doc_id>', methods=['GET', 'PUT'])
+    belongs_to_current_user = doc.user == current_user
+    
+    return render_template('view_doc.html', doc=doc, belongs_to_current_user=belongs_to_current_user)
+    
+    
+
+@app.route('/docs/<int:doc_id>/json')
+def get_json(doc_id):
+    doc = YamlDocument.query.get_or_404(doc_id) 
+    return doc.json, {'Content-type':'application/json'}
+
+
+
+    
+@app.route('/docs/<int:doc_id>/edit', methods=['GET', 'PUT'])
 @login_required    
 def edit_yaml_doc(doc_id):
     
-    doc = YamlDocument.query.get(doc_id)
+    doc = YamlDocument.query.get_or_404(doc_id)
     
     if doc.user != current_user:
         return 'to do: better response here'
@@ -39,7 +58,7 @@ def edit_yaml_doc(doc_id):
         try:
             json_string = process_yaml(form.document.data)
         except InvalidYaml as e:
-            form.errors['document'].append(e.msg)
+            form.errors['document'].append(e.msg) # can i do this?
             valid = False
         else:
             valid = True
@@ -49,17 +68,8 @@ def edit_yaml_doc(doc_id):
         form.populate_obj(doc)
         doc.json = json_string
         db.session.commit()
+        flash('yaml document saved', 'alert-success')
         return redirect(url_for(view_yaml_doc, doc_id=doc_id))
                 
     
     return render_template('edit_doc.html', form=form)
-    
-    
-    
-
-
-
-    
-
-    
-    
